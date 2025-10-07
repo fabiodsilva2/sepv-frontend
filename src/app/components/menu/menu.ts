e5ba0e4842c6da20ca8cc7b7a37489c2619f0c3b
Fábio Damasceno Silva <fabiodamasceno4@hotmail.com>
2025-10-07 20:23:04 -0300

    Ajustando tamanho do botão do usuário e colocado menu suspenso

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-menu',
-  imports: [MatToolbarModule,MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule,MatButtonModule, MatIconModule,MatMenuModule],
   templateUrl: './menu.html',
   styleUrl: './menu.css'
 })
 export class Menu {
+nameUSer: any='Fábio Damasceno Silva';
+signOut() {
+throw new Error('Method not implemented.');
+}
 
 }
