2154e3372f94f12190f3be92f4983c1627ed9d05
Fábio Damasceno Silva <fabiodamasceno4@hotmail.com>
2025-10-09 22:37:13 -0300

    Entendendo a lógica do sidenav

--- src/app/components/layout/layout.ts
+++ src/app/components/layout/layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Menu } from '../menu/menu';
 import { MenuLateral } from '../menu-lateral/menu-lateral';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet,Menu,MenuLateral],
+  imports: [RouterOutlet,Menu,MenuLateral,MatSidenavModule],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
