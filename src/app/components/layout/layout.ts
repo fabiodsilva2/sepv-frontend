d2d3818da4fb875ec97b29f5470fa05c4acd9ce2
Fábio Damasceno Silva <fabiodamasceno4@hotmail.com>
2025-10-09 22:53:42 -0300

    Ajeitando o menu

--- src/app/components/layout/layout.ts
+++ src/app/components/layout/layout.ts
@@ -3,10 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { Menu } from '../menu/menu';
 import { MenuLateral } from '../menu-lateral/menu-lateral';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet,Menu,MenuLateral,MatSidenavModule],
+  imports: [RouterOutlet,Menu,MatSidenavModule,MatIconModule,MatMenuModule, MatSidenavModule,MatListModule ],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
